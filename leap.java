8d81ae6b91cdb11e0db389d327fc37e4ddf288e4
Gitm5 <martin2006stephen@gmail.com>
2025-07-19 22:52:28 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class leap {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        long leapyear;
+        System.out.println("enter the year");
+        leapyear=sc.nextLong();
+        if((leapyear%4==0&&leapyear%100!=0)||(leapyear%400==0)){
+             System.out.println("it is a leap year");
+        }
+        else{
+            System.out.println("it is not a leap year");
+        }
+
+
+
+    }
+}
