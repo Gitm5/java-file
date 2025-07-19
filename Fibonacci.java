316c62d1109b7828b75e656686c5b1fc1bdc13d1
Gitm5 <martin2006stephen@gmail.com>
2025-07-19 22:51:17 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n,c,i=1;
+        System.out.println("enter the limit");
+        n=sc.nextInt();
+        int a=0;
+        int b=1;
+        System.out.println("fibonacci series :");
+        while (i<=n) {
+             System.out.print(a + " "); 
+            i++;
+            c=a+b;
+            
+            b=a;
+            a=c;
+           
+            
+
+          
+        }
+        
+     }
+    
+}
